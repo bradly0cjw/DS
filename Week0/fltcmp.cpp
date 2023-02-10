ae9fb4d08a7ddeabe72b594cd9ab7869952c58c3
bradly0cjw <bradly093@gmail.com>
2023-02-10 21:31:54 +0800

    cpp idea in mind

---
+++ Week0/fltcmp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    float x;
+    cin >> a;
+    while(cin >> x){
+        x=1/x;
+        cout<<x;
+    }
+    return 0;
+}
+
+    
