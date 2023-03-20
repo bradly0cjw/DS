70d5057fdfabccbb7a623e1aed33f6fb659daa9f
bradly0cjw <bradly093@gmail.com>
2023-03-20 16:50:18 +0800

    wu fuck my com puter

---
+++ test.py
@@ -0,0 +1,12 @@
+while True:
+    n=int(input())
+    f1=1
+    f2=1
+    if n==1 or n==2:
+        print(1)
+    else:
+        for i in range(n-2):
+            temp=f1+f2
+            f1=f2
+            f2=temp
+        print(temp)
